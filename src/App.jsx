90c0390c494fd6f05a7a16bcba619bec914c5443
Shashanka naik <134037764+Shashanka-22@users.noreply.github.com>
2024-03-24 02:21:16 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './components/Navbar'
+import Maneger from './components/Maneger'
+import Footer from './components/Footer'
+
+function App() {
+
+
+  return (
+    <>
+      <Navbar />
+      <Maneger />
+      <Footer/>
+    </>
+  )
+}
+
+export default App
