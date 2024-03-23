4ddc6998bbd96c6572f6ecbac8ccf995314f906c
Shashanka naik <134037764+Shashanka-22@users.noreply.github.com>
2024-03-24 02:22:00 +0530

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white flex flex-col justify-center items-center  w-full'>
+            <div className="logo font-bold text-2xl">
+                <span className='text-green-500'>&lt;</span>
+
+                Pass
+                <span className='text-green-500'>OP/ &gt;</span>
+
+            </div>
+            <div className='flex font-bold justify-center items-center'>Created with <img className='w-7 mx-2' src="icons/heart.png" alt="" /> by Shanka
+            </div>
+        </div>
+    )
+}
+
+export default Footer
